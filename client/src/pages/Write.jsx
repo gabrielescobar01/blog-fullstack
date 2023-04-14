acdff70a4b9d426bbec0e452a2750aa46a9849eb
Gabriel Escobar <claudioescobardg31@gmail.com>
2023-04-14 02:01:34 -0300

    add api dir

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,53 +1,76 @@
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   return (
-    <div className='add'>
-      <div className='content'>
-        <input type='text' placeholder='Title' />
-        <div className='editorContainer'>
-          <ReactQuill className='editor' theme='snow' value={value} onChange={setValue} />;
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+          ;
         </div>
       </div>
-      <div className='menu'>
-        <div className='item'>
+      <div className="menu">
+        <div className="item">
           <n1>Publish</n1>
           <span>
             <b>Status: </b> Draft
           </span>
           <span>
-            <b>Visibility: </b> Public 
+            <b>Visibility: </b> Public
           </span>
-          <input style={{display:"none"}} type='file' name='' id='file' />
-          <label className='file' htmlFor='file'>Upload Image</label>
-          <div className='buttons'>
+          <input style={{ display: "none" }} type="file" name="" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
             <button>Save as a draft</button>
             <button>Update</button>
           </div>
         </div>
-        <div className='item'>
+        <div className="item">
           <h1>Category</h1>
-          <input type='radio' name='cat' value="art" id='art' />
-          <label htmlFor='art'>Art</label>
-          <input type='radio' name='cat' value="science" id='science' />
-          <label htmlFor='science'>Science</label>
-          <input type='radio' name='cat' value="technology" id='technology' />
-          <label htmlFor='technology'>Technology</label>
-          <input type='radio' name='cat' value="cinema" id='cinema' />
-          <label htmlFor='cinema'>Cinema</label>
-          <input type='radio' name='cat' value="design" id='design' />
-          <label htmlFor='design'>Design</label>
-          <input type='radio' name='cat' value="food" id='food' />
-          <label htmlFor='food'>Food</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="science" id="science" />
+            <label htmlFor="science">Science</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="technology" id="technology" />
+            <label htmlFor="technology">Technology</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="design" id="design" />
+            <label htmlFor="design">Design</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="food" id="food" />
+            <label htmlFor="food">Food</label>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
